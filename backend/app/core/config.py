536f9b75ae1bc4fb2cb008a67621221ca487821a
Jamova01 <jorgemova01@gmail.com>
2025-05-22 16:18:55 -0500

    feat(cors): configure CORS middleware and origin settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -36,19 +36,19 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     ALGORITHM: str
-    # FRONTEND_HOST: str = "http://localhost:5173"
+    FRONTEND_HOST: str = "http://localhost:5173"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
-    # BACKEND_CORS_ORIGINS: Annotated[
-    #     list[AnyUrl] | str, BeforeValidator(parse_cors)
-    # ] = []
+    BACKEND_CORS_ORIGINS: Annotated[list[AnyUrl] | str, BeforeValidator(parse_cors)] = (
+        []
+    )
 
-    # @computed_field  # type: ignore[prop-decorator]
-    # @property
-    # def all_cors_origins(self) -> list[str]:
-    #     return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
-    #         self.FRONTEND_HOST
-    #     ]
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
+            self.FRONTEND_HOST
+        ]
 
     PROJECT_NAME: str
     SENTRY_DSN: HttpUrl | None = None
