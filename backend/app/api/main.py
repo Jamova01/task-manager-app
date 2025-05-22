e69965f16a9978beb79641a93524da591edff535
Jamova01 <jorgemova01@gmail.com>
2025-05-22 13:40:15 -0500

    Add tasks router to main API router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, users
+from app.api.routes import login, users, tasks
 
 
 api_router = APIRouter()
 
 api_router.include_router(login.router)
 api_router.include_router(users.router)
+api_router.include_router(tasks.router)
