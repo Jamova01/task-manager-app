9f7cb1345d1f1971f2dd4452cb32f470f5d018a4
Jamova01 <jorgemova01@gmail.com>
2025-05-22 16:17:05 -0500

    feat(cors): implement CORS middleware with dynamic origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,7 @@ from fastapi.routing import APIRoute
 
 from app.api.main import api_router
 from app.core.config import settings
+from starlette.middleware.cors import CORSMiddleware
 
 
 def custom_generate_unique_id(route: APIRoute) -> str:
@@ -22,6 +23,15 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
 )
+
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
